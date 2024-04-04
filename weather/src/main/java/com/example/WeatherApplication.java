2c45d276bc6df2c40e0c07787b1a4dc57d87ac80
imloadinqqq <imloadinq7579@gmail.com>
2024-04-04 12:17:02 -0500

    Added Basic GUI elements needed, will place later

---
+++ weather/src/main/java/com/example/WeatherApplication.java
@@ -0,0 +1,52 @@
+package com.example;
+
+import javafx.application.Application;
+import javafx.fxml.FXMLLoader;
+import javafx.scene.Parent;
+import javafx.scene.Scene;
+import javafx.stage.Stage;
+import javafx.scene.control.Label;
+import javafx.scene.control.TextField;
+import javafx.scene.layout.GridPane;
+import javafx.scene.shape.Box;
+
+
+import java.io.IOException;
+
+/**
+ * JavaFX App
+ */
+public class WeatherApplication extends Application {
+
+    private static Scene scene;
+
+    @Override
+    public void start(Stage stage) throws IOException {
+        GridPane gridPane = new GridPane();
+        Label location = new Label("Location");
+        Label temp = new Label("Temp");
+        Label h = new Label("H");
+        Label l = new Label("L");
+        Label search = new Label("Search");
+        TextField searchField = new TextField();
+        Label desc = new Label("Desc.");
+        Label descInfo = new Label("Desc. Info");
+        Label forecast = new Label("Forecast");
+        Box forecastBox = new Box();
+        forecastBox.setHeight(50);
+        forecastBox.setWidth(150);
+
+        Scene scene = new Scene(gridPane, 500, 300);
+
+        gridPane.getChildren().addAll(location, temp, h, l, search, searchField, desc, descInfo, forecast,forecastBox);
+
+        stage.setTitle("Weather");
+        stage.setScene(scene);
+        stage.show();
+    }
+    
+    public static void main(String[] args) {
+        launch(args);
+    }
+
+}
